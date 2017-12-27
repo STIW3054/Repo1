86e1cffa2a6b4d531c6fe2fa68d3fe6d759c271e
goaykailing <31922634+goaykailing@users.noreply.github.com>
2017-12-28 06:58:18 +0800

    Upload src/test

--- src/test/java/compileJava/CompileWorkerTest.java
+++ src/test/java/compileJava/CompileWorkerTest.java
@@ -1,5 +1,35 @@
 package compileJava;
 
+import determineFile.FileExecutors;
+import java.util.ArrayList;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import retrieveRepo.GitRepo;
+import setupDirectory.TargetRoot;
+
 public class CompileWorkerTest {
     
+    private String filePath;
+    
+    public void testingCompileWorker(){
+        
+        TargetRoot tar = new TargetRoot();
+        String reposMainFolder = tar.getTarget();
+        GitRepo repo = new GitRepo();
+        ArrayList<String> matricNums = repo.getMatrics();
+         
+        FileExecutors file = new FileExecutors(reposMainFolder, matricNums);
+        ArrayList<String> fileNames = file.getJavaFiles();
+        
+        CompileExecutors com = new CompileExecutors(fileNames);
+
+        this.filePath = filePath;
+        CompileWorker cw = new CompileWorker(filePath);
+    }
+    
+    @Test
+    public void testCompileWorker(){
+        boolean actualResult = true;
+        assertTrue(true);
+    }
 }
--- src/test/java/setupDirectory/TargetRootTest.java
+++ src/test/java/setupDirectory/TargetRootTest.java
@@ -1,5 +1,16 @@
 package setupDirectory;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class TargetRootTest {
     
+    @Test
+    public void testTargetRoot(){
+        TargetRoot tr = new TargetRoot();
+        String fileSeperator = System.getProperty("file.separator");        
+        String expectedTR =  System.getProperty("user.home") + fileSeperator +"Desktop" + fileSeperator +"STIW3054-A171";
+        
+        assertEquals(expectedTR, tr.getTarget());
+    }
 }
